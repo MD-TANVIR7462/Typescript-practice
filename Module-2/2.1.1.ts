310922082c034c78aface0fabf716f4c9ea7802a
unknown <mdtanvir7462@gmail.com>
2023-11-11 16:26:17 +0600

    type assertion

---
+++ Module-2/2.1.1.ts
@@ -0,0 +1,17 @@
+{
+ //type Assertion
+ 
+ 
+ const data = (value :any)  => {
+ const Type = typeof value
+ return Type
+
+ }
+ const num = data (10) 
+const str = data("hellow")
+
+console.log({num},{str});
+
+
+//
+}
