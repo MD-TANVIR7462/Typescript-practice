712a7b4a61a9bc3656a7be660bd248e246f0596a
unknown <mdtanvir7462@gmail.com>
2023-11-11 15:38:38 +0600

    how to use type

--- Module-2/2.1.ts
+++ Module-2/2.1.ts
@@ -1,1 +1,21 @@
-const word:string = "hellow world "
+type Students = {
+   name: string;
+   height: string;
+   roll: number;
+   id: number;
+   adress: {
+     village: string;
+     city?: string;
+   };
+ } 
+
+const student: Students = {
+  name: "Tanvir",
+  height: "5 foot 6 inches",
+  roll: 1,
+  id: 1221,
+  adress: {
+    village: "Chadpur",
+    city: "Chadpur",
+  },
+};
